e9846fc83be7e405143e60fb3250c12b9b74b18f
Tanya92 <tatiana_mironchik@tut.by>
2019-03-18 15:36:42 +0300

    feat: create function makeExchange

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    const cash = {
+      "H": 50,
+      "Q": 25,
+      "D": 10,
+      "N": 5,
+      "P": 1
+    };
+    let exchange = {};
+    if (currency <= 0) {
+      return {};
+    } else if (currency > 10000) {
+      return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+      for (let i in cash) {
+        exchange[i] = Math.floor(currency / cash[i]);
+        currency = currency - exchange[i] * cash[i];
+        if (exchange[i] == 0) {
+          delete exchange[i];
+        }
+      }
+    return exchange;
+    }
 }
